9f697bae0dad005942cd56a4f2456733c6b91270
Omago <miguel.perassoli@gmail.com>
2025-04-02 23:43:47 -0300

    Add files via upload

---
+++ php_teste/php.php
@@ -0,0 +1,25 @@
+
+<div style= "">
+<?php
+
+
+
+$idade = $_GET["idade"];
+$nome = $_GET["nome"];
+$altura = $_GET["altura"];
+$sexo = $_GET["sexo"];
+
+
+
+echo "nome ". $nome;
+echo "<br>";
+echo "altura ". $altura;
+echo "<br>";
+echo "idade ". $idade;
+echo "<br>";
+echo "sexo ". $sexo;
+echo "<br>";
+
+
+?>
+</div>
